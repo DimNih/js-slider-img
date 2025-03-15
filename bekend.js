fea334d3bbd81f4961448015355fb660354eec48
Dimas Nih <dimaserlansyah5@gmail.com>
2025-03-15 22:35:13 +0700

    Update

--- bekend.js
+++ bekend.js
@@ -20,3 +20,29 @@ document.querySelector('.prev').addEventListener('click', () => {
 function updateSlide() {
     slides.style.transform = `translateX(-${index * 25}%)`; /*   jarak slide   */
 }
+
+
+// test
+
+/* let index = 0;
+const slides = document.querySelector('.slides');
+const images = document.querySelectorAll('.slides img');
+const totalSlides = images.length;
+
+document.querySelector('.next').addEventListener('click', () => {
+    if (index < totalSlides - 1) {
+        index++;
+        updateSlide();
+    }
+});
+
+document.querySelector('.prev').addEventListener('click', () => {
+    if (index > 0) {
+        index--;
+        updateSlide();
+    }
+});
+
+function updateSlide() {
+    slides.style.transform = `translateX(-${index * 25}%)`; /*   jarak slide   */
+} 
